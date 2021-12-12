9b46760291bbba908fa39bd6e08de96812866acd
Pedro Thiago Roque <pedronmroque@gmail.com>
2021-12-12 02:42:25 -0300

    Builder funcionando

---
+++ builder.py
@@ -0,0 +1,4 @@
+import os
+#os.system(r' "C:\Program Files\Unity\Hub\Editor\2018.4.35f1\Editor\unity.exe" -quit -batchmode -executeMethod BuilderLinhaComando.PerformBuild ') #os.system(' ') roda linhas de comando no cmd
+os.system(' C:\\"Program Files"\\Unity\\Hub\\Editor\\2018.4.35f1\\Editor\\unity.exe  -batchmode -quit -projectPath D:\Projetos\GEGRADI\Motivacao-Builder-LOCAL -executeMethod BuilderLinhaComando.PerformBuild') #os.system(' ') roda linhas de comando no cmd
+exit()
