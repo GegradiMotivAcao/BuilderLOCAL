8b2c9ead8ab917fb3b252b867c82443420010653
PedroThiagoRoque <pedronmroque@gmail.com>
2023-10-28 14:53:51 -0300

    Atualização da rotação de imagens

---
+++ Assets/Scripts/rotacionaImagem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class rotacionaImagem : MonoBehaviour
+{
+   public Button rotateButton;
+
+    void Start()
+    {
+        rotateButton.onClick.AddListener(RotateRight90Degrees);
+    }
+
+    void RotateRight90Degrees()
+    {
+        transform.Rotate(0, 0, -90);
+    }
+}
